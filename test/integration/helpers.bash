ab674f1f9cb35d1823e240d5f7aa7154bc184720
bodymindarts <justin@galoy.io>
2022-02-21 21:23:35 +0100

    test: reliable network setup

--- test/integration/helpers.bash
+++ test/integration/helpers.bash
@@ -2,8 +2,24 @@
 
 lnctl=./target/debug/lnctl
 
+FAUCET_PRIVATE_KEY="92Qba5hnyWSn5Ffcka56yMQauaWY6ZLd91Vzxbi4a9CCetaHtYj"
+FAUCET="mgWUuj1J1N882jmqFxtDepEC73Rr22E9GU"
+
 start_network() {
-  docker compose up -d
+  docker compose up -d bitcoind
+  genblocks
+  docker compose up integration-deps
+  lnd1_pubkey=$(lnd_cmd lnd1 getinfo | jq -r '.identity_pubkey')
+  lnd2_pubkey=$(lnd_cmd lnd2 getinfo | jq -r '.identity_pubkey')
+
+  cache_value "lnd1_pubkey" "$lnd1_pubkey"
+  cache_value "lnd2_pubkey" "$lnd2_pubkey"
+
+  lnd_cmd lnd1 connect "${lnd2_pubkey}@lnd2:9735"
+}
+
+cache_value() {
+  echo $2 > ${BATS_TMPDIR}/$1
 }
 
 teardown_network() {
@@ -23,6 +39,16 @@ stop_lnctl() {
   kill $(cat $(test_tmp_dir)/lnctl_pid) > /dev/null
 }
 
+lnd_cmd() {
+  container="${1}"
+  shift
+  docker compose exec -T ${container} lncli -n regtest ${@}
+}
+
+genblocks() {
+  docker compose exec -T bitcoind bitcoin-cli -regtest generatetoaddress 250 ${FAUCET}
+}
+
 # Run the given command in the background. Useful for starting a
 # node and then moving on with commands that exercise it for the
 # test.
@@ -35,4 +61,27 @@ stop_lnctl() {
 background() {
   "$@" 3>- &
   echo $!
+}
+
+# Stolen from
+# https://github.com/docker/swarm/blob/master/test/integration/helpers.bash
+retry() {
+  local attempts=$1
+  shift
+  local delay=$1
+  shift
+  local i
+
+  for ((i=0; i < attempts; i++)); do
+    run "$@"
+    # shellcheck disable=2154
+    if [[ "$status" -eq 0 ]] ; then
+      return 0
+    fi
+    sleep "$delay"
+  done
+
+  # shellcheck disable=2154
+  echo "Command \"$*\" failed $attempts times. Output: $output"
+  false
 }
