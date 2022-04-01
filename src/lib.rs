9198ffb0252a9f68ed4ba2355204cce000972469
bodymindarts <justin@galoy.io>
2022-04-01 10:46:46 +0200

    Remove old src

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod cli;
-
-mod client;
-mod config;
-mod grpc;
-mod lnd;
-mod message_forwarder;
-mod node;
-mod uncertainty_graph;
--- src/main.rs
+++
@@ -1,6 +0,0 @@
-use lnctl::cli;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    cli::run().await
-}
